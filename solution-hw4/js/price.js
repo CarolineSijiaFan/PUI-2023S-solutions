0154842445cb3e2f39d8a4ac45fcc295666104bc
Sijia Fan <79704029+CarolineSijiaFan@users.noreply.github.com>
2023-02-21 17:29:37 -0500

    Revise Based on HW3

--- solution-hw4/js/price.js
+++ solution-hw4/js/price.js
@@ -1,22 +1,53 @@
 console.log("Price JavaScript is working!");
 
-glazeVal = 0;
-sizeVal = 1;
-const curGlazing = document.querySelector('#glazing');
-const curSize = document.querySelector('#size');
+const glazingPrices = {
+	"Keep original" : 0.0,
+	"Sugar milk" : 0.0,
+	"Vanilla milk" : 0.50,
+	"Double chocolate" : 1.50
+};
 
+const packPrices = {
+	"1" : 1, "3" : 3, "6" : 5, "12" : 10
+};
 
-function updatePrice() {
-    if (glazeVal != curGlazing.value){
-        glazeVal = curGlazing.value;};
-    if (sizeVal != curSize.value){
-        sizeVal = curSize.value;};
+const basePrice = 2.49;
+let glazingOption = "Keep original";
+let packOption = 1;
 
-    value = (2.49 + parseFloat(glazeVal))*sizeVal;
+let glazingPrice = 0;
+let packPrice = 1;
 
-    const curPrice = document.querySelector('#box1');
-    console.log(curGlazing.value);
-    curPrice.innerText = "$" + value.toFixed(2);
+const glazingSelect = document.querySelector("#glazing");
+
+for (const [pack, price] of Object.entries(glazingPrices)) {
+	const option = document.createElement("option");
+	option.textContent = pack;
+	option.value = price;
+	glazingSelect.appendChild(option);
 }
 
-document.addEventListener('input', updatePrice,false);
+const sizeSelect = document.querySelector("#size");
+
+for (const [pack, price] of Object.entries(packPrices)) {
+	const option = document.createElement("option");
+	option.textContent = pack;
+	option.value = price;
+	sizeSelect.appendChild(option);
+}
+
+function glazingChange(element) {
+	glazingPrice = parseFloat(element.value);
+    updateTotalPrice();
+}
+
+function sizeChange(element) {
+	packPrice = parseFloat(element.value);
+    updateTotalPrice();
+}
+
+function updateTotalPrice() {
+	const totalPrice = (basePrice + glazingPrice) * packPrice;
+    const totalPriceField = document.querySelector("#box1");
+    totalPriceField.innerText = "$" + totalPrice.toFixed(2);
+}
