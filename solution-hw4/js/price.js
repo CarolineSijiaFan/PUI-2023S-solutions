3e47b45acf72835a22dc65082829dce9ef4e1cd9
Sijia Fan <79704029+CarolineSijiaFan@users.noreply.github.com>
2023-02-21 19:00:27 -0500

    Finish HW 4

--- solution-hw4/js/price.js
+++ solution-hw4/js/price.js
@@ -1,5 +1,46 @@
 console.log("Price JavaScript is working!");
 
+const rolls = {
+    "Original": {
+        "basePrice": 2.49,
+        "imageFile": "original-cinnamon-roll.jpg"
+    },
+    "Apple": {
+        "basePrice": 3.49,
+        "imageFile": "apple-cinnamon-roll.jpg"
+    },
+    "Raisin": {
+        "basePrice": 2.99,
+        "imageFile": "raisin-cinnamon-roll.jpg"
+    },
+    "Walnut": {
+        "basePrice": 3.49,
+        "imageFile": "walnut-cinnamon-roll.jpg"
+    },
+    "Double-Chocolate": {
+        "basePrice": 3.99,
+        "imageFile": "double-chocolate-cinnamon-roll.jpg"
+    },
+    "Strawberry": {
+        "basePrice": 3.99,
+        "imageFile": "strawberry-cinnamon-roll.jpg"
+    }    
+};
+
+
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
+const chosenRoll = params.get('roll')
+
+const title = document.querySelector("#title")
+title.innerText = chosenRoll + " Cinnamon Rolls";
+
+const image = document.querySelector(".detailImage");
+const imageLocation = "products/"+rolls[chosenRoll].imageFile;
+image.src = imageLocation;
+
+const basePrice = rolls[chosenRoll].basePrice;
+
 const glazingPrices = {
 	"Keep original" : 0.0,
 	"Sugar milk" : 0.0,
@@ -11,9 +52,10 @@ const packPrices = {
 	"1" : 1, "3" : 3, "6" : 5, "12" : 10
 };
 
-const basePrice = 2.49;
 let glazingOption = "Keep original";
 let packOption = 1;
+
+let totalPrice = basePrice;
 
 let glazingPrice = 0;
 let packPrice = 1;
@@ -37,17 +79,46 @@ for (const [pack, price] of Object.entries(packPrices)) {
 }
 
 function glazingChange(element) {
+    glazingOption = element.options[element.selectedIndex].text;
 	glazingPrice = parseFloat(element.value);
     updateTotalPrice();
 }
 
 function sizeChange(element) {
+    packOption = element.options[element.selectedIndex].text;
 	packPrice = parseFloat(element.value);
     updateTotalPrice();
 }
 
 function updateTotalPrice() {
-	const totalPrice = (basePrice + glazingPrice) * packPrice;
+	totalPrice = (basePrice + glazingPrice) * packPrice;
+    totalPrice = totalPrice.toFixed(2);
     const totalPriceField = document.querySelector("#box1");
-    totalPriceField.innerText = "$" + totalPrice.toFixed(2);
+    totalPriceField.innerText = "$" + totalPrice;
 }
+
+updateTotalPrice();
+
+class Roll {
+
+    constructor(rollType, rollGlazing, packSize, basePrice) {
+        this.type = rollType;
+        this.glazing =  rollGlazing;
+        this.size = packSize;
+        this.basePrice = basePrice;
+    }
+}
+
+const cart = new Set();
+
+function addProduct(type,glazing,size,price){
+    const product = new Roll(type,glazing,size,price);
+    cart.add(product);
+    console.log(cart);
+    return product
+}
+
+const addInCart = document.querySelector(".box2");
+addInCart.addEventListener('click', () => {
+    addProduct(chosenRoll,glazingOption,packOption,totalPrice);
+});
